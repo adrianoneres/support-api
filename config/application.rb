147a3d830a3d7903a421ced00dbbc14ea4ab8056
Adriano Neres <adrianoneres@users.noreply.github.com>
2017-09-07 23:44:25 -0400

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module SupportApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
       end
     end
     config.api_only = true
